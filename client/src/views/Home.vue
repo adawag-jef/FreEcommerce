3d616d94f286c3f98fd2b82b3e3aafb74ecaa95e
Jeffrey Adawag <jeffrey@tirsolutions.com>
2020-10-05 15:52:06 +0800

    Home page feature product

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -95,134 +95,52 @@
     </div>
 
     <div class="mt-16">
-      <h3 class="text-gray-600 text-2xl font-medium">Fashions</h3>
+      <h3 class="text-gray-600 text-2xl font-medium">Featured Products</h3>
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <div
-          class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-        >
+        <template v-for="featured in featuredProducts">
           <div
-            class="flex items-end justify-end h-56 w-full bg-cover"
-            style="background-image: url('https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80')"
+            :key="featured._id"
+            class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
           >
-            <button
-              class="p-2 rounded-full bg-green-600 text-white mx-5 -mb-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
+            <!-- <pre>{{ featured }}</pre> -->
+            <div
+              class="flex items-end justify-end h-56 w-full bg-cover"
+              :style="`background-image: url(${featured.product_id.mainPhoto})`"
             >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+              <button
+                class="p-2 rounded-full bg-green-600 text-white mx-5 -mb-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
               >
-                <path
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div class="px-5 py-3">
-            <h3 class="text-gray-700 uppercase">Chanel</h3>
-            <span class="text-gray-500 mt-2">$12</span>
-          </div>
-        </div>
-        <div
-          class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-        >
-          <div
-            class="flex items-end justify-end h-56 w-full bg-cover"
-            style="background-image: url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')"
-          >
-            <button
-              class="p-2 rounded-full bg-green-600 text-white mx-5 -mb-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+                <svg
+                  class="h-5 w-5"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div class="px-5 py-3">
+              <h3 class="text-gray-700 uppercase">
+                {{ featured.product_id.name }}
+              </h3>
+              <span class="text-gray-500 mt-2"
+                >${{ featured.product_id.price }}</span
               >
-                <path
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                ></path>
-              </svg>
-            </button>
+            </div>
           </div>
-          <div class="px-5 py-3">
-            <h3 class="text-gray-700 uppercase">Man Mix</h3>
-            <span class="text-gray-500 mt-2">$12</span>
-          </div>
-        </div>
-        <div
-          class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-        >
-          <div
-            class="flex items-end justify-end h-56 w-full bg-cover"
-            style="background-image: url('https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')"
-          >
-            <button
-              class="p-2 rounded-full bg-green-600 text-white mx-5 -mb-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div class="px-5 py-3">
-            <h3 class="text-gray-700 uppercase">Classic watch</h3>
-            <span class="text-gray-500 mt-2">$12</span>
-          </div>
-        </div>
-        <div
-          class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-        >
-          <div
-            class="flex items-end justify-end h-56 w-full bg-cover"
-            style="background-image: url('https://images.unsplash.com/photo-1590664863685-a99ef05e9f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=345&q=80')"
-          >
-            <button
-              class="p-2 rounded-full bg-green-600 text-white mx-5 -mb-4 hover:bg-green-500 focus:outline-none focus:bg-green-500"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <div class="px-5 py-3">
-            <h3 class="text-gray-700 uppercase">woman mix</h3>
-            <span class="text-gray-500 mt-2">$12</span>
-          </div>
-        </div>
+        </template>
       </div>
     </div>
 
-    <div class="mt-16">
+    <!-- <div class="mt-16">
       <h3 class="text-gray-600 text-2xl font-medium">Fashions</h3>
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
@@ -348,12 +266,22 @@
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters({
+      featuredProducts: "product/getFeaturedProducts",
+    }),
+  },
+  mounted() {
+    this.$store.dispatch("product/listFeaturedProducts");
+  },
+};
 </script>
 
 <style></style>
